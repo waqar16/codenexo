366095bcc24ba8d953e6fd5fe8dc3fe5e7d3180d
saimm <saimshehzad2030@gmail.com>
2026-01-13 12:11:34 +0500

    scripts added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,15 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PRC8MZ58');`,
+          }}
+        />
+        <script
           type="application/ld+json"
           // Organization Schema
           dangerouslySetInnerHTML={{
@@ -75,6 +84,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PRC8MZ58"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {/* <SplashCursor/> */}
 
         {children}
